257fa984dd869164372761a6b6a22d620ae759a6
Luca Lischetti <luca@sirlisko.com>
2020-07-10 09:06:41 +0100

    Add link account (#22)

---
+++ pages/api/link-account.js
@@ -0,0 +1,36 @@
+import { getLinkedAccount, linkAccount, unlinkAccount } from "lib/api/accounts";
+
+export default async (req, res) => {
+  const { accountNumber, cssoId, linkId } = req.query;
+  switch (req.method) {
+    case "GET":
+      try {
+        const data = await getLinkedAccount({ cssoId });
+        res.status(200).json(data);
+      } catch (error) {
+        console.log("Get Link Account error:", error);
+        res.status(500).json(error);
+      }
+      break;
+    case "POST":
+      try {
+        const data = await linkAccount({ accountNumber, cssoId });
+        res.status(200).json(data);
+      } catch (error) {
+        console.log("Link Account error:", error);
+        res.status(500).json(error);
+      }
+      break;
+    case "DELETE":
+      try {
+        await unlinkAccount({ linkId });
+        res.status(204).end();
+      } catch (error) {
+        console.log("Unlink Account error:", error);
+        res.status(500).json(error);
+      }
+      break;
+    default:
+      res.status(400).json("Invalid request method");
+  }
+};
