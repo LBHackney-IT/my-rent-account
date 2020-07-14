7f8a7c13bd690a0d01b3be24d53fdb1fafd1a0ce
Luca Lischetti <luca@sirlisko.com>
2020-07-14 22:41:19 +0200

    decoupled AccountLogin form

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useForm } from "react-hook-form";
 import Router from "next/router";
-import isPostcodeValid from "uk-postcode-validator";
 import { getSession, setSession } from "lib/session";
 
 import ErrorMessage from "components/ErrorMessage/ErrorMessage";
-import { Button, TextInput } from "components/Form";
 
 export default function Home() {
   const [error, setError] = useState();
@@ -43,38 +40,12 @@ export default function Home() {
         To view your rent account balance and make a payment, please enter your
         rent account number and post code in the boxes below.
       </p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextInput
-          label="Rent Account Number:"
-          hint="You will find this on your rent card or statement"
-          name="accountNumber"
-          placeholder="e.g. 1234567890"
-          inputClassName="govuk-input--width-10"
-          inputMode="numeric"
-          error={errors.accountNumber}
-          register={register({
-            required: "Rent Account Number is required",
-            pattern: {
-              value: /^[0-9]+$/,
-              message: "Account Number must be a number",
-            },
-          })}
-        />
-        <TextInput
-          label="Post Code:"
-          name="postcode"
-          placeholder="e.g. E8 1EA"
-          inputClassName="govuk-input--width-10"
-          inputMode="numeric"
-          error={errors.postcode}
-          register={register({
-            required: "Post Code is required",
-            validate: (value) =>
-              isPostcodeValid(value) || "You need a valid post code",
-          })}
-        />
-        <Button text="View account" disabled={submitting} />
-      </form>
+
+      <AccountLogin
+        onSubmit={onSubmit}
+        submitText="View account"
+        isSubmitting={submitting}
+      />
       {error && <ErrorMessage text={error} />}
     </div>
   );
