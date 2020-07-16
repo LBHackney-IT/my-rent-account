88138cadfdb9a2eb8330e65001192787a9445e1a
Luca Lischetti <luca@sirlisko.com>
2020-07-16 08:36:24 +0100

    Improved error handling link account and normalized postcode (#29)

--- components/AccountLogin/AccountLogin.jsx
+++ components/AccountLogin/AccountLogin.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import { useForm } from "react-hook-form";
+import Router from "next/router";
 import isPostcodeValid from "uk-postcode-validator";
 
 import { Button, TextInput } from "components/Form";
+import ErrorMessage from "components/ErrorMessage/ErrorMessage";
 
-const AccountLogin = ({ onSubmit, submitText, isSubmitting }) => {
+const AccountLogin = ({ onSubmit, onAsyncSubmit, submitText }) => {
+  const [submitting, setSubmitting] = useState();
   const { register, errors, handleSubmit } = useForm();
+  const [error, setError] = useState();
+  const onLogin = async (params) => {
+    try {
+      setSubmitting(true);
+      setError();
+      const account = await axios.get("/api/accounts", { params });
+      await axios.post("/api/audit", { ...params, ...account });
+      onSubmit && onSubmit(params);
+      onAsyncSubmit && (await onAsyncSubmit(params));
+      Router.push("/account");
+    } catch (e) {
+      console.error(e);
+      setError(
+        e.response.status === 404 ? (
+          <div>
+            <p>
+              This rent account number and postcode combination is incorrect.
+            </p>
+            Please check if you have entered the correct information.
+          </div>
+        ) : (
+          e.response.data
+        )
+      );
+    }
+    setSubmitting(false);
+  };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onLogin)}>
       <TextInput
         label="Rent Account Number:"
         hint="You will find this on your rent card or statement"
@@ -41,15 +72,16 @@ const AccountLogin = ({ onSubmit, submitText, isSubmitting }) => {
             isPostcodeValid(value) || "You need a valid post code",
         })}
       />
-      <Button text={submitText} disabled={isSubmitting} />
+      <Button text={submitText} disabled={submitting} />
+      {error && <ErrorMessage text={error} />}
     </form>
   );
 };
 
 AccountLogin.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func,
+  onAsyncSubmit: PropTypes.func,
   submitText: PropTypes.string.isRequired,
-  isSubmitting: PropTypes.bool,
 };
 
 export default AccountLogin;
