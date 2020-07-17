c5849717e85ca4bb44345a92dac7ca4a9e3c5c58
Luca Lischetti <luca@sirlisko.com>
2020-07-17 10:00:43 +0200

    fixed scroll on top on route change

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import App from "next/app";
+import Router from "next/router";
 
 import "./stylesheets/all.scss";
 
 import Layout from "components/Layout/Layout";
 
 export default class MyApp extends App {
+  componentDidMount() {
+    Router.events.on("routeChangeComplete", () => {
+      window.scrollTo(0, 0);
+    });
+  }
+
   render() {
     const { Component, pageProps } = this.props;
     return (
