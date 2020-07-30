145ec766e18eb35b485f47f6f4d72e7701893732
Wayne Greeff <61699419+LBH-wgreeff@users.noreply.github.com>
2020-07-30 22:11:02 +0100

    added google analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "lib/gtag";
 
 export default class AppDocument extends Document {
   render() {
     return (
       <Html className="govuk-template lbh-template">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body className="govuk-template__body lbh-template__body js-enabled">
           <Main />
           <NextScript />
