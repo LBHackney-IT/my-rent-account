01b72de0cc3db624f7bb26dae0d62adc242050fb
Jayson Hunter <github@jaysonhunter.com>
2020-08-21 16:43:17 +0100

    Bugfix/mra 311 fix gsso v3 (#47)
    * Remove code to debug too many redirects from login
    
    * Debug attempts

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { checkIsAdmin } from "lib/admin-session";
-
 import AdminNavBar from "components/AdminNavBar/AdminNavBar";
 
 export default function AdminHome() {
