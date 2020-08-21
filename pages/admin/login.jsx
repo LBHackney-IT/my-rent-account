827776125a7eccc90ebbb682a929cb47346eba4a
Jayson Hunter <github@jaysonhunter.com>
2020-08-21 11:26:09 +0100

    Feature/mra 311 update gsso (#44)
    * Add lib function to return protocol
    
    * Update session lib to export redirectToHome function
    
    * Update adminSession to redirect to home
    
    * Add protocol to login returnUrl var

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { checkIsAdmin } from "lib/admin-session";
-
+import { getProtocol } from "lib/urls";
 import AdminLogin from "components/AdminLogin/AdminLogin";
 
 export default function AdminLoginPage({ gssoUrl, returnUrl }) {
   return (
     <div>
       <h1>Login to My Rent Account</h1>
+
       <p className="govuk-body">
         This page is to log in to service team member accounts.
       </p>
@@ -28,8 +29,9 @@ AdminLoginPage.propTypes = {
 
 export const getServerSideProps = async (ctx) => {
   const { GSSO_URL } = process.env;
-  const host = ctx.req.headers.host;
+  const baseUrl = ctx.req.headers.host;
   const account = checkIsAdmin(ctx, false);
+  const protocol = getProtocol();
 
   if (account && account.isAdmin) {
     ctx.res.writeHead(302, {
@@ -41,7 +43,7 @@ export const getServerSideProps = async (ctx) => {
   return {
     props: {
       gssoUrl: GSSO_URL,
-      returnUrl: `${host}/admin`,
+      returnUrl: `${protocol}://${baseUrl}/admin/login`,
     },
   };
 };
