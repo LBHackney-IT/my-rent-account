7036a6aed529c0b4a222038d0d87861e8be24580
Jayson Hunter <github@jaysonhunter.com>
2020-08-25 17:03:15 +0100

    Update home page to cater for admin logged in.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { getSession, setSession } from "lib/session";
+import { getSession, updateSession } from "lib/session";
 
 import AccountLogin from "components/AccountLogin/AccountLogin";
 
 const { CSSO_DOMAIN, CSSO_ID, CSSO_SECRET, URL_PREFIX } = process.env;
 
-export default function Home({ loginUrl, registerUrl }) {
+export default function Home({ isAdmin, loginUrl, registerUrl }) {
   return (
     <div>
       <h1>My rent account</h1>
@@ -14,7 +14,11 @@ export default function Home({ loginUrl, registerUrl }) {
         To view your rent account balance and make a payment, please enter your
         rent account number and post code in the boxes below.
       </p>
-      <AccountLogin onSubmit={setSession} submitText="View account" />
+      <AccountLogin
+        isAdmin={isAdmin}
+        onSubmit={updateSession}
+        submitText="View account"
+      />
       <hr className="govuk-section-break govuk-section-break--l govuk-section-break--visible" />
       <h2>Login to One Account</h2>
       <p className="govuk-body">
@@ -45,13 +49,16 @@ export default function Home({ loginUrl, registerUrl }) {
 }
 
 Home.propTypes = {
+  isAdmin: PropTypes.bool,
   registerUrl: PropTypes.string.isRequired,
   loginUrl: PropTypes.string.isRequired,
 };
 
 export const getServerSideProps = async (ctx) => {
   const account = getSession(ctx, false);
-  if (account && !account.isAdmin) {
+  const isAdmin = Boolean(account.isAdmin);
+
+  if (account && !isAdmin) {
     ctx.res.writeHead(302, {
       Location:
         account.accountNumber || account.isAdmin ? "/account" : "/link-account",
@@ -61,6 +68,7 @@ export const getServerSideProps = async (ctx) => {
   const queryString = `?grant_type=authorization_code&client_id=${CSSO_ID}&client_secret=${CSSO_SECRET}&scope=openid%20email&response_type=code&redirect_uri=https://${URL_PREFIX}/auth`;
   return {
     props: {
+      isAdmin: isAdmin,
       registerUrl: `${CSSO_DOMAIN}/users/sign_up${queryString}`,
       loginUrl: `${CSSO_DOMAIN}/oauth/authorize${queryString}`,
     },
