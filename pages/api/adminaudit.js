b5ff8872a74783a630a9da391b38817f7457866a
Jayson Hunter <github@jaysonhunter.com>
2020-08-26 12:31:38 +0100

    New admin audit api page

---
+++ pages/api/adminaudit.js
@@ -0,0 +1,31 @@
+import { postAdminAudit } from "lib/api/audit";
+
+export default async (req, res) => {
+  switch (req.method) {
+    case "POST":
+      try {
+        console.log("pages/api/adminaudit.js");
+        const body = req.body;
+
+        const adminAuditParams = {
+          user: body.user,
+          accountNumber: body.accountNumber,
+          cssoLogin: body.simulateCSSO ? true : false,
+          auditAction: body.auditAction || "VIEW",
+        };
+
+        console.log(adminAuditParams);
+
+        await postAdminAudit(adminAuditParams);
+        res.status(204).end();
+      } catch (error) {
+        console.log(error.status);
+        console.log("Admin audit error:", error);
+        res.status(500).json("Unable to post Admin Audit");
+      }
+      break;
+
+    default:
+      res.status(400).json("Invalid request method");
+  }
+};
